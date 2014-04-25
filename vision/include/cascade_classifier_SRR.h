a4d3e03eb95f68bd4baade929b219159dd09ecff
Aero <aero@aero-OptiPlex-9010.(none)>
2014-04-25 17:37:44 -0400

    fix

--- vision/include/cascade_classifier_SRR.h
+++ vision/include/cascade_classifier_SRR.h
@@ -7,6 +7,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <time.h>
+#include <image_geometry/stereo_camera_model.h>
+#include <stereo_msgs/DisparityImage.h>
 
 
 static const std::string OPENCV_WINDOW_RIGHT = "Top_Right_Rectified";
@@ -23,17 +25,35 @@ private:
      //ROS related vars.
      ros::NodeHandle nh_;
      image_transport::ImageTransport it_;
-     image_transport::Subscriber m_image_sub_left;
+     image_transport::CameraSubscriber m_image_sub_left;
+     image_transport::CameraSubscriber m_image_sub_right;
+     //image_transport::Subscriber m_disp_sub;
      image_transport::Publisher  m_image_pub_left;
+	sensor_msgs::CameraInfo left_info;
+	sensor_msgs::CameraInfo right_info;
+     	cv_bridge::CvImagePtr m_disp_ptr;
+message_filters::Subscriber<stereo_msgs::DisparityImage> m_disp_sub; 
 
 	
     // CV related vars
 	cv::CascadeClassifier cascade_WHA;
     char* m_LeftCameraView;
+    	image_geometry::StereoCameraModel stereo_model;
+
+	// other vars
+	bool m_leftCbTrig;
+	bool m_rightCbTrig;
+	bool m_stereoModelNotConfigured;
 	
 	// Private methods
 	void m_detectAndDisplay(const cv_bridge::CvImagePtr& cv_ptr, cv::Mat& frame, const char* WINDOW);
-	void m_imageCb(const sensor_msgs::ImageConstPtr& msg);
+	void m_dispCb(const stereo_msgs::DisparityImage::ConstPtr& msg);
+	void m_imageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info);
+	void m_imageCbRight(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info);
+	void m_computeDisparityPoint(double x, double y);
+	void m_configStereoModel();
+
+
 	
 
 };
--- vision/src/cascade_classifier_SRR.cpp
+++ vision/src/cascade_classifier_SRR.cpp
@@ -4,11 +4,13 @@ using namespace std;
 using namespace cv;
 using namespace sensor_msgs;
 
-cascade_classifier_node::cascade_classifier_node(): it_(nh_), m_LeftCameraView("Top_Left_Rectified")
+cascade_classifier_node::cascade_classifier_node(): it_(nh_), m_LeftCameraView("Top_Left_Rectified"), m_leftCbTrig(false), m_rightCbTrig(false), m_stereoModelNotConfigured(true)
     {
         // Subscribe to input video feed and publish output video feed
      //   m_image_sub_left = it_.subscribe("camera/image", 1, &cascade_classifier_node::m_imageCb, this);
-	m_image_sub_left = it_.subscribe("/stereo/left/image_raw",1, &cascade_classifier_node::m_imageCb, this);
+	m_image_sub_left = it_.subscribeCamera("/stereo/left/image_raw",1, &cascade_classifier_node::m_imageCb, this);
+	m_image_sub_right = it_.subscribeCamera("/stereo/right/image_raw",1, &cascade_classifier_node::m_imageCbRight, this);
+        m_disp_sub = it_.subscribe("/stereo/disparity", 1, &cascade_classifier_node::m_dispCb, this);
        // m_image_pub_left = it_.advertise("/stereo/left/image_rect_small", 1);
         cv::namedWindow(m_LeftCameraView);
     }
@@ -51,18 +53,89 @@ void cascade_classifier_node::m_detectAndDisplay(const cv_bridge::CvImagePtr& cv
                           cv::Point(center.x + WHA_faces[i].width / 2,
                                 center.y + WHA_faces[i].height / 2),
                           cv::Scalar(255, 0, 0));
+
+		m_computeDisparityPoint(center.x, center.y);
         }
         cv::line(frame,cv::Point2d(0,HORIZON),cv::Point2d(frame_gray.cols,HORIZON),cv::Scalar(0,255,0));
         cv::imshow(m_LeftCameraView, frame);
         cv::waitKey(3);
 
     }
-
-void cascade_classifier_node::m_imageCb(const sensor_msgs::ImageConstPtr& msg)
+void cascade_classifier_node::m_configStereoModel()
+{
+	this->stereo_model.fromCameraInfo(this->left_info, this->right_info);
+}
+void cascade_classifier_node::m_computeDisparityPoint(double x, double y)
     {
-// i++;
-// if (i>10)
-//{
+   	
+   	cv::Mat disp_frame;
+	disp_frame = m_disp_ptr->image;
+	Point2d obj_centroid(x,y);
+	Point3d obj_3d;
+	float disp_val = disp_frame.at<float>(obj_centroid.y, obj_centroid.x);
+	this->stereo_model.projectDisparityTo3d(obj_centroid, disp_val,
+obj_3d);
+
+	cout << "Detection at "<< obj_3d.x << "," << obj_3d.y << "," << obj_3d.z <<endl;
+
+	
+
+    }
+
+void cascade_classifier_node::m_dispCb(const stereo_msgs::DisparityImage::ConstPtr& msg)
+    {
+        	
+   
+            static cv::Mat frame;
+	    
+            try
+            {
+                m_disp_ptr = cv_bridge::toCvCopy(msg->image, sensor_msgs::image_encodings::BGR8);
+            }
+            catch (cv_bridge::Exception& e)
+            {
+                ROS_ERROR("cv_bridge exception: %s", e.what());
+                return;
+            }
+
+
+    }
+
+void cascade_classifier_node::m_imageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
+    {
+	m_leftCbTrig = true;
+        	left_info = *cam_info;
+            cv_bridge::CvImagePtr cv_ptr;
+            static cv::Mat frame;
+
+            try
+            {
+                cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+            }
+            catch (cv_bridge::Exception& e)
+            {
+                ROS_ERROR("cv_bridge exception: %s", e.what());
+                return;
+            }
+		if(m_leftCbTrig && m_rightCbTrig && m_stereoModelNotConfigured)
+		{
+			m_stereoModelNotConfigured = false;
+			m_configStereoModel();
+		}	
+
+
+            // Update GUI Window
+	    if(!m_stereoModelNotConfigured)
+            m_detectAndDisplay(cv_ptr,frame, m_LeftCameraView);
+
+    }
+
+
+
+void cascade_classifier_node::m_imageCbRight(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
+    {
+	m_rightCbTrig = true;
+	    right_info = *cam_info;	
             cv_bridge::CvImagePtr cv_ptr;
             static cv::Mat frame;
 
@@ -76,18 +149,10 @@ void cascade_classifier_node::m_imageCb(const sensor_msgs::ImageConstPtr& msg)
                 return;
             }
 
+	
 
-
-            // Update GUI Window
-            m_detectAndDisplay(cv_ptr,frame, m_LeftCameraView);
-//}
 
     }
-
-
-//};
-
-
 
 int main(int argc, char** argv)
 {
