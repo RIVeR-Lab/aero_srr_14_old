74e769d2d2e9c7437178246f986f6c0fc7a4bbe2
Mitchell Wills <mwills@wpi.edu>
2014-05-10 13:50:07 -0400

    Made cascade classifier node spin single threaded

--- vision/src/cascade_classifier_node.cpp
+++ vision/src/cascade_classifier_node.cpp
@@ -5,7 +5,5 @@ int main(int argc, char **argv){
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
   vision::CascadeClassifier classifier(nh, pnh);
-  //ros::spin();
-  ros::MultiThreadedSpinner spinner(4);
-  spinner.spin();
+  ros::spin();
 }
