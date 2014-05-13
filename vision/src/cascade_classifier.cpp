8b4f4f6867b7b449c77724c5b7f55466207780c2
Mitchell Wills <mwills@wpi.edu>
2014-05-12 20:58:58 -0400

    Fixed unfinished merge

--- vision/src/cascade_classifier.cpp
+++ vision/src/cascade_classifier.cpp
@@ -229,15 +229,3 @@ namespace vision{
 
 
 }
-<<<<<<< HEAD
-
-
-int main(int argc, char **argv){
-  ros::init(argc, argv, "cascade_classifier");
-  ros::NodeHandle nh;
-  ros::NodeHandle pnh("~");
-  vision::CascadeClassifier classifier(nh, pnh);
-  ros::spin();
-}
-=======
->>>>>>> vision-arm
